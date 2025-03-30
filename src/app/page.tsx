2ca40683071d55ed6f94574f6b0d793b39b34dab
asankaab <asanka.abewickrama@gmail.com>
2025-03-31 00:22:29 +0530

    Add global styles, header component, and new icons; update dependencies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import styles from "./page.module.css";
 import Heading1 from "@/components/ui/Heading1";
 import Heading2 from "@/components/ui/Heading2";
 
@@ -7,9 +6,11 @@ export default async function Home() {
 
   return (
     <main>
-      <Heading1>Making Every Moment Unforgettable.</Heading1>
-      <Heading2>Welcome to our world of photography!</Heading2>
-      <p>Explore the interplay of light and shadow to create dramatic, moody, or ethereal photographs.</p>
+      <div className="wrapper">
+        <Heading1>Making Every Moment Unforgettable.</Heading1>
+        <Heading2>Welcome to our world of photography!</Heading2>
+        <p>Explore the interplay of light and shadow to create dramatic, moody, or ethereal photographs.</p>
+      </div>
     </main>
   );
 }
