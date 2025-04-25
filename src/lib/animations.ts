0ff221940e95ba9158efe20a5904319c76b15ffc
asankaab <asanka.abewickrama@gmail.com>
2025-04-25 20:42:25 +0530

    Enhance header animations and styles; add fadeIn animation, update link hover effect, and adjust color opacity in header styles.

--- src/lib/animations.ts
+++ src/lib/animations.ts
@@ -9,3 +9,7 @@ export const slideUp: AnimationParams = {
     y: { from: 30, to: 0 }, 
     opacity: { from: 0, to: 1 },
 }
+
+export const fadeIn: AnimationParams = {  
+    opacity: { from: 0, to: 1 },
+}
